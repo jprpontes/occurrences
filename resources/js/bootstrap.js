a8baa53dc7ca610bf0c5e5414e897641c5ab06b1
João Paulo Ribeiro Pontes <joaopauloribeiropontes@gmail.com>
2021-05-02 23:59:48 -0300

    Interceptor no axios.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -22,7 +22,14 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
+window.axios.interceptors.response.use(response => {
+    return Promise.resolve(response);
+ }, error => {
+   if (error.response.status === 401) {
+        window.location = '/login';
+   }
+   return Promise.reject(error);
+ });
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
